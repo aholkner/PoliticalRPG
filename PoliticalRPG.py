443f171761e9662607c73067b88a015ffa803975
Alex Holkner <alex.holkner@gmail.com>
2014-05-11 15:52:24 +1000

    Multiple worlds

--- PoliticalRPG.py
+++ PoliticalRPG.py
@@ -40,6 +40,32 @@ class World(object):
         self.tile_size = map.tile_width
         self.sprites = []
         self.camera_x = self.camera_y = 0
+        self.player_sprite = None
+
+        for layer in self.map.layers[:]:
+            if 'sprite' in layer.properties:
+                self.map.layers.remove(layer)
+                for i, image in enumerate(layer.images):
+                    if image:
+                        x = i % self.map.rows
+                        y = i / self.map.rows
+                        self.add_sprite(image, x, y)
+
+    def add_sprite(self, image, x, y):
+        sprite = Sprite(image, x, y)
+        self.sprites.append(sprite)
+
+        if hasattr(image, 'properties') and 'player' in image.properties:
+            self.player_sprite = sprite
+
+    def get_sprite_at(self, x, y):
+        for sprite in self.sprites:
+            if sprite.x == x and sprite.y == y:
+                return sprite
+        return None
+
+    def update(self):
+        pass
 
     def draw(self):
         ts = self.tile_size
@@ -61,13 +87,14 @@ class World(object):
         bacon.pop_transform()
 
     def on_key_pressed(self, key):
-        if key in input_movement:
-            dx, dy = input_movement[key]
-            self.move(dx, dy)
+        pass
 
-    def move(self, dx, dy):
-        player.x += dx
-        player.y += dy
+class MapWorld(World):
+    def __init__(self, map):
+        super(MapWorld, self).__init__(map)
+
+    def update(self):
+        self.update_camera()
         
     def update_camera(self):
         ts = self.tile_size
@@ -75,28 +102,54 @@ class World(object):
         # Viewport in scaled space
         window_width = bacon.window.width / self.scale
         window_height = bacon.window.height / self.scale
-        self.camera_x = player.x * ts - window_width / 2
-        self.camera_y = player.y * ts - window_height / 2
+        self.camera_x = self.player_sprite.x * ts - window_width / 2
+        self.camera_y = self.player_sprite.y * ts - window_height / 2
         self.camera_x = clamp(self.camera_x, 0, self.map.tile_width * self.map.cols - window_width)
         self.camera_y = clamp(self.camera_y, 0, self.map.tile_height * self.map.rows - window_height)
 
-bacon.window.width = 256
-bacon.window.height = 256
+    def on_key_pressed(self, key):
+        if key in input_movement:
+            dx, dy = input_movement[key]
+            self.move(dx, dy)
 
-map = tiled.parse('res/map.tmx')
+    def move(self, dx, dy):
+        other = self.get_sprite_at(self.player_sprite.x + dx, self.player_sprite.y + dy)
+        if other:
+            self.on_collide(other)
+        else:
+            self.player_sprite.x += dx
+            self.player_sprite.y += dy
+        
+    def on_collide(self, other):
+        game.push_world(MapWorld(tiled.parse('res/combat.tmx')))
+
+bacon.window.width = 512
+bacon.window.height = 512
+
 spritesheet = Spritesheet('res/spritesheet.txt')
-player = Sprite(spritesheet.images['player'], 1, 1)
-world = World(map)
-world.sprites.append(player)
 
 class Game(bacon.Game):
+    def __init__(self):
+        self.world = None
+        self.world_stack = []
+
+    def push_world(self, world):
+        self.world_stack.append(self.world)
+        self.world = world
+
+    def pop_world(self):
+        self.world = self.world_stack.pop()
+
     def on_tick(self):
         bacon.clear(0, 0, 0, 1)
-        world.update_camera()
-        world.draw()
+        self.world.update()
+        self.world.draw()
 
     def on_key(self, key, pressed):
         if pressed:
-            world.on_key_pressed(key)
+            self.world.on_key_pressed(key)
 
-bacon.run(Game())
+game = Game()
+game.world = MapWorld(tiled.parse('res/map.tmx'))
+
+bacon.run(game)
---
+++ res/sprites.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="sprites" tilewidth="8" tileheight="8">
+ <image source="sprites.png" width="64" height="48"/>
+ <tile id="0">
+  <properties>
+   <property name="player" value="1"/>
+  </properties>
+ </tile>
+</tileset>
