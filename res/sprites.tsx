790ef9be6a590cfd9b4356e690aec04cb26601b4
Alex Holkner <alex.holkner@gmail.com>
2014-05-15 14:20:43 +1000

    Character sprites

--- res/sprites.tsx
+++ res/sprites.tsx
@@ -13,17 +13,117 @@
  </tile>
  <tile id="2">
   <properties>
-   <property name="character" value="SecretService001"/>
+   <property name="character" value="Lobbyist001"/>
   </properties>
  </tile>
  <tile id="3">
   <properties>
-   <property name="character" value="HippyGirl001"/>
+   <property name="character" value="Bumpkin001"/>
   </properties>
  </tile>
  <tile id="4">
   <properties>
    <property name="character" value="ReligiousNut001"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="character" value="HippyGirl001"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="character" value="Receptionist001"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="character" value="Protester001"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="character" value="Journalist001"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="character" value="Constituent001"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="character" value="Mayor"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="character" value="Governor"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="character" value="DeepThroat"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="character" value="TVPersonality001"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="character" value="President"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="character" value="PresidentAlien"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="character" value="Unionist001"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="character" value="Teacher001"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="character" value="WarVet001"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="character" value="CabinetPolitician001"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="character" value="JointChief001"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="character" value="Wife"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="character" value="Leo"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="character" value="SecretAgent"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="character" value="Computer"/>
   </properties>
  </tile>
  <tile id="40">
