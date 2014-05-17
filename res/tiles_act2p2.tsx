43234b207cc354a5f24e08af58290d5875548959
Alex Holkner <alex.holkner@gmail.com>
2014-05-17 22:46:59 +1000

    Collision hotel

---
+++ res/tiles_act2p2.tsx
@@ -0,0 +1,195 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles_act2p2" tilewidth="8" tileheight="8">
+ <image source="tiles_act2p2.png" width="64" height="64"/>
+ <terraintypes>
+  <terrain name="Exterior" tile="-1"/>
+  <terrain name="Interior" tile="-1"/>
+ </terraintypes>
+ <tile id="0" terrain="1,1,1,0">
+  <properties>
+   <property name="c" value=""/>
+  </properties>
+ </tile>
+ <tile id="1" terrain="1,1,0,0">
+  <properties>
+   <property name="c" value=""/>
+  </properties>
+ </tile>
+ <tile id="2" terrain="1,1,0,1">
+  <properties>
+   <property name="c" value=""/>
+  </properties>
+ </tile>
+ <tile id="3" terrain="0,0,0,1">
+  <properties>
+   <property name="c" value=""/>
+  </properties>
+ </tile>
+ <tile id="4" terrain="0,0,1,1">
+  <properties>
+   <property name="c" value=""/>
+  </properties>
+ </tile>
+ <tile id="5" terrain="0,0,1,0">
+  <properties>
+   <property name="c" value=""/>
+  </properties>
+ </tile>
+ <tile id="8" terrain="1,0,1,0">
+  <properties>
+   <property name="c" value=""/>
+  </properties>
+ </tile>
+ <tile id="9" terrain="0,0,0,0"/>
+ <tile id="10" terrain="0,1,0,1">
+  <properties>
+   <property name="c" value=""/>
+  </properties>
+ </tile>
+ <tile id="11" terrain="0,1,0,1">
+  <properties>
+   <property name="c" value=""/>
+  </properties>
+ </tile>
+ <tile id="12" terrain="1,1,1,1"/>
+ <tile id="13" terrain="1,0,1,0">
+  <properties>
+   <property name="c" value=""/>
+  </properties>
+ </tile>
+ <tile id="16" terrain="1,0,1,1">
+  <properties>
+   <property name="c" value=""/>
+  </properties>
+ </tile>
+ <tile id="17" terrain="0,0,1,1">
+  <properties>
+   <property name="c" value=""/>
+  </properties>
+ </tile>
+ <tile id="18" terrain="0,1,1,1">
+  <properties>
+   <property name="c" value=""/>
+  </properties>
+ </tile>
+ <tile id="19" terrain="0,1,0,0">
+  <properties>
+   <property name="c" value=""/>
+  </properties>
+ </tile>
+ <tile id="20" terrain="1,1,0,0">
+  <properties>
+   <property name="c" value=""/>
+  </properties>
+ </tile>
+ <tile id="21" terrain="1,0,0,0">
+  <properties>
+   <property name="c" value=""/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="c" value="lu"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="c" value="u"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="c" value="ur"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="c" value="d"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="c" value="l"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="c" value="r"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="c" value="r"/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="c" value="l"/>
+  </properties>
+ </tile>
+ <tile id="40">
+  <properties>
+   <property name="c" value="dl"/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="c" value="d"/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="c" value="dr"/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <properties>
+   <property name="c" value="u"/>
+  </properties>
+ </tile>
+ <tile id="48">
+  <properties>
+   <property name="c" value="dr"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="c" value="d"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="c" value=""/>
+  </properties>
+ </tile>
+ <tile id="52">
+  <properties>
+   <property name="c" value=""/>
+  </properties>
+ </tile>
+ <tile id="53">
+  <properties>
+   <property name="c" value=""/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="c" value="dl"/>
+  </properties>
+ </tile>
+ <tile id="59">
+  <properties>
+   <property name="c" value=""/>
+  </properties>
+ </tile>
+ <tile id="60">
+  <properties>
+   <property name="c" value=""/>
+  </properties>
+ </tile>
+ <tile id="61">
+  <properties>
+   <property name="c" value=""/>
+  </properties>
+ </tile>
+</tileset>
