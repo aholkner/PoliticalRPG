4e7f3ff9b69eeca374a674bba597d536cdfe1396
Alex Holkner <alex.holkner@gmail.com>
2014-05-17 14:43:06 +1000

    act 2 tiles

---
+++ res/tiles_act1.tsx
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles_act1" tilewidth="8" tileheight="8">
+ <image source="tiles_act1.png" width="64" height="64"/>
+ <terraintypes>
+  <terrain name="Interior" tile="-1"/>
+  <terrain name="Exterior" tile="-1"/>
+ </terraintypes>
+ <tile id="0" terrain="1,1,1,0"/>
+ <tile id="1" terrain="1,1,0,0"/>
+ <tile id="2" terrain="1,1,0,1"/>
+ <tile id="3" terrain="0,0,0,1"/>
+ <tile id="4" terrain="0,0,1,1"/>
+ <tile id="5" terrain="0,0,1,0"/>
+ <tile id="8" terrain="1,0,1,0"/>
+ <tile id="9" terrain="0,0,0,0"/>
+ <tile id="10" terrain="0,1,0,1"/>
+ <tile id="11" terrain="0,1,0,1"/>
+ <tile id="12" terrain="1,1,1,1"/>
+ <tile id="13" terrain="1,0,1,0"/>
+ <tile id="16" terrain="1,0,1,1"/>
+ <tile id="17" terrain="0,0,1,1"/>
+ <tile id="18" terrain="0,1,1,1"/>
+ <tile id="19" terrain="0,1,0,0"/>
+ <tile id="20" terrain="1,1,0,0"/>
+ <tile id="21" terrain="1,0,0,0"/>
+</tileset>
